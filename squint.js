b86eb7a9feb9b3a7b1d2e6cad254fc49ec6c0787
Pixelbandito <pixelbandito+ask@gmail.com>
2023-01-22 00:15:43 -0600

    Update icons and styles, add in-extension preview

--- squint.js
+++ squint.js
@@ -1,13 +1,13 @@
 function getGrayScalePercent() {
-    const grayScalePercent = parseInt(document.querySelector('#grayScaleRange').value, 10);
-    console.log(grayScalePercent);
-    return grayScalePercent;
+    return parseInt(document.querySelector('#grayScaleRange').value, 10);
 }
+
 function getBlurPx() {
-    const blurPx = parseInt(document.querySelector('#blurRange').value, 10);
-    console.log(blurPx);
-    return blurPx;
+    return parseInt(document.querySelector('#blurRange').value, 10);
 }
+
+document.querySelector('#grayScaleRange').addEventListener('change', updatePreviewStyles);
+document.querySelector('#blurRange').addEventListener('change', updatePreviewStyles);
 
 document.querySelector('#applyButton').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -16,39 +16,48 @@ document.querySelector('#applyButton').addEventListener('click', () => {
 
         chrome.scripting.executeScript({
             target : {tabId : currTab.id},
-            func : updateStyles,
+            func : updateHostStyles,
             args : [ getGrayScalePercent(), getBlurPx() ],
         });
     });    
 });
 
 document.querySelector('#resetButton').addEventListener('click', () => {
+    document.querySelector('#grayScaleRange').value = '50';
+    document.querySelector('#blurRange').value = '5';
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var currTab = tabs[0];
         if (!currTab) return;
 
         chrome.scripting.executeScript({
             target : {tabId : currTab.id},
-            func : updateStyles,
+            func : updateHostStyles,
             args : [ 0, 0 ],
             });
       });    
 });
 
-function updateStyles(grayScalePercent = 100, blurPx = 1) {
-    console.log('BLAMMO', document);
-    document.querySelectorAll('[data-controlled-by-chrome-extension="squint"]').forEach(el => el.remove());
+function writeStyleContents(grayScalePercent = 100, blurPx = 1) {
+    return `
+        -webkit-filter: grayscale(${grayScalePercent}%) blur(${blurPx}px) !important; /* Safari 6.0 - 9.0 */
+        filter: grayscale(${grayScalePercent}%) blur(${blurPx}px) !important;
+`;
+}
 
+function updatePreviewStyles() {
+    const styleEl = document.querySelector('#style');
+    styleEl.innerHTML = `.preview {${writeStyleContents(getGrayScalePercent(), getBlurPx())}}`;
+}
+
+function updateHostStyles(grayScalePercent = 100, blurPx = 1) {
+    document.querySelectorAll('[data-controlled-by-chrome-extension="squint"]').forEach(el => el.remove());
     const styleEl = document.createElement('style');
     styleEl.id = 'chrome-ext-squint-el';
     styleEl.setAttribute('data-controlled-by-chrome-extension', 'squint');
-    
-    styleEl.innerHTML = `
-    html {
+    styleEl.innerHTML = `html {
         -webkit-filter: grayscale(${grayScalePercent}%) blur(${blurPx}px) !important; /* Safari 6.0 - 9.0 */
         filter: grayscale(${grayScalePercent}%) blur(${blurPx}px) !important;
-    }
-    `;
-    
+    }`;
     document.body.append(styleEl);
 }
